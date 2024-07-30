7e9bf18fbc3268852e9214b43c9015b7d9ca91d6
Keerthana R <148705349+Starplayevryr@users.noreply.github.com>
2024-07-30 15:26:40 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int topo[10],k;
+void dfs(int a[10][10],int n,int visited[10],int source)
+{
+    int i;
+    visited[source]=1;
+    for(i=1;i<=n;i++)
+    {
+        if(a[source][i]==1 && visited[i]==0)
+            dfs(a,n,visited,i);
+    }
+    topo[++k]=source;
+
+}
 int main()
 {
-    printf("Hello world!\n");
+    int n,a[10][10],i,j,visited[10];
+    printf("\n enter the number of nodes:");
+    scanf("%d",&n);
+    printf("\n Read adjacency matrix:\n");
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            scanf("%d",&a[i][j]);
+         }
+     }
+    for(i=1;i<=n;i++)
+    {
+        visited[i]=0;
+    }
+    for(i=1;i<=n;i++)
+    {
+        if(visited[i]==0)
+        {
+            dfs(a,n,visited,i);
+
+        }
+    }
+    printf("\n Topological ordering is \n");
+    for(i=k;i>=1;i--)
+        printf("%d\t",topo[i]);
     return 0;
 }
+
